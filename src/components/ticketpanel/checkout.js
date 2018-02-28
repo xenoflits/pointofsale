ba98c77de549f71251be0ed5aef464e96b6b1e5d
unknown <soeradj.mahabiersing@gmail.com>
2018-02-28 12:16:19 +0100

    worked more on checkout

--- src/components/ticketpanel/checkout.js
+++ src/components/ticketpanel/checkout.js
@@ -1,30 +1,45 @@
 import React from 'react'
+import fiveeuro from '../../images/5euro.png'
+import teneuro from '../../images/10euro.png'
+import twentyeuro from '../../images/20euro.png'
+import fiftyeuro from '../../images/50euro.png'
+import otherpayments from '../../images/betaal.gif'
 
 class Checkout extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             checkoutvalue: 0
         }
     }
-    render(){
-        return(
-            <div className="checkout"> 
-            <div className="amount"><input className="amount-input"></input></div>
-            <div className="checkout-nr">
-            <Button nr="1" />
-            <Button nr="2" />
-            <Button nr="3" />
-            <Button nr="4" />
-            <Button nr="5" />
-            <Button nr="6" />
-            <Button nr="7" />
-            <Button nr="8" />
-            <Button nr="9" />
-            <Button nr="x" />
-            <Button nr="0" />
-            <Button nr="ok" />
-            </div>
+    render() {
+        return (
+            <div className="checkout">
+                <div className="checkout-left">
+                    <div className="amount"><input className="amount-input"></input></div>
+                    <div className="checkout-nr">
+                        <Button nr="1" />
+                        <Button nr="2" />
+                        <Button nr="3" />
+                        <Button nr="4" />
+                        <Button nr="5" />
+                        <Button nr="6" />
+                        <Button nr="7" />
+                        <Button nr="8" />
+                        <Button nr="9" />
+                        <Button nr="x" />
+                        <Button nr="0" />
+                        <Button nr="ok" />
+                        <img className="other-p" src={otherpayments} alt="otherpaymentoptions" />
+                    </div>
+                </div>
+                <div className="checkout-right">
+                    <img className="bills" src={fiveeuro} alt="5euro" />
+                    <img className="bills" src={teneuro} alt="10euro" />
+                    <img className="bills" src={twentyeuro} alt="20euro" />
+                    <img className="bills" src={fiftyeuro} alt="50euro" />
+
+                </div>
             </div>
         )
     }
--- src/components/ticketpanel/ticket.js
+++ src/components/ticketpanel/ticket.js
@@ -1,63 +1,69 @@
 import React from 'react'
 import TicketItem from './ticketitem'
-import CheckOut from './checkout'
 
 
+//this component handles the ticket 
 class Ticket extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             order: 1,
-            
         }
-    
+        this.newOrder = this.newOrder.bind(this);
+    }
+    //this method checks if the total amount is 0, else it proceeds to the checkout screen
+    newOrder(subtotal) {
+        if (subtotal === 0) {
+            alert("Can't checkout when the bill is 0")
+        }
+        else {
+            this.props.neworder();
+        }
     }
 
- 
-    render(){
-      let items = this.props.items;
-   
-        const result = [...items.reduce( (mp, o) => {
+    render() {
+        let items = this.props.items;
+        //this check for duplicate items on the ticket and merges them with the quantity property
+        const result = [...items.reduce((mp, o) => {
             if (!mp.has(o.name)) mp.set(o.name, Object.assign({ quantity: 0 }, o));
             mp.get(o.name).quantity++;
             return mp;
         }, new Map).values()];
-        
-        console.log(result);
-      
-        
-        let orderdisplay = result.map((order, i)=>{return <TicketItem key = {i} quantity={order.quantity} name={order.name} cost={order.cost*order.quantity}/>})
+        //this generates the items on the tickets and calculates the total cost per item
+        let orderdisplay = result.map((order, i) => { return <TicketItem key={i} quantity={order.quantity} name={order.name} cost={order.cost * order.quantity} /> })
+        //this calculates the total amount on the ticket
         let subtotal = 0;
-        result.map((order,i)=>{
-            subtotal = subtotal+(order.cost*order.quantity);
+        result.map((order, i) => {
+            subtotal = subtotal + (order.cost * order.quantity);
             return null
         })
+        //this makes the total amount a float
         const roundedTotal = parseFloat(subtotal).toFixed(2);
-        return(
-        <div className="ticket">
-        
-            <div className="ticket-header">
-                <h1>Order: {this.props.ordernr}</h1>
-                <h1>Waiter: Soeradj Mahabiersing</h1>
+        return (
+            <div className="ticket">
+
+                <div className="ticket-header">
+                    <h1>Order: {this.props.ordernr}</h1>
+                    <h1>Waiter: Soeradj Mahabiersing</h1>
+                </div>
+
+                <table>
+                    <tbody>
+                        {orderdisplay}
+                        <tr className="tr-total">
+                            <td></td>
+                            <td>Total</td>
+                            <td>{roundedTotal}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div className="buttons">
+                    <button onClick={() => this.newOrder(subtotal)} className="checkout-button">Checkout</button>
+                    <button onClick={this.props.cancel} className="cancel-button">Cancel</button>
+                    <button className="queue-button">Queue</button>
+                </div>
             </div>
-            
-            <table>
-               <tbody>
-                    {orderdisplay}
-                    <tr className="tr-total">
-                        <td></td>
-                        <td>Total</td>
-                        <td>{roundedTotal}</td>
-                    </tr>
-                </tbody>
-            </table>
-        
-            <div className="buttons">
-                <button onClick={this.props.neworder} className="checkout-button">Checkout</button>
-                <button onClick={this.props.cancel} className="cancel-button">Cancel</button>
-                <button className="queue-button">Queue</button>
-            </div>
-        </div>
         )
     }
 }
